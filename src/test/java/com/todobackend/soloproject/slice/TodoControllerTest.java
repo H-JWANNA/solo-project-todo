16ba6c4be6ead9d2582ddf0354cac1804fd9b972
H-JWANNA <hongjjwan@gmail.com>
2022-12-18 21:13:54 +0900

    style: apply code convention

--- src/test/java/com/todobackend/soloproject/slice/TodoControllerTest.java
+++ src/test/java/com/todobackend/soloproject/slice/TodoControllerTest.java
@@ -59,35 +59,22 @@ public class TodoControllerTest {
 		String content = gson.toJson(post);
 
 		// when : MockMvc 객체로 테스트 대상 Controller 호출
-		ResultActions actions =
-			mockMvc.perform(post("/")
-				.accept(MediaType.APPLICATION_JSON)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(content));
+		ResultActions actions = mockMvc.perform(
+			post("/").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(content));
 
 		// then : Controller 메서드에서 응답으로 수신한 HTTP Status 및 Response Body를 통한 검증
-		actions
-			.andExpect(status().isCreated())
+		actions.andExpect(status().isCreated())
 			.andExpect(jsonPath("$.title").value(post.getTitle()))
 			.andExpect(jsonPath("$.todoOrder").value(post.getTodoOrder()))
 			.andExpect(jsonPath("$.completed").value(post.getCompleted()))
-			.andDo(document(
-					"post-todo",
-					getRequestPreProcessor(),
-					getResponsePreProcessor(),
-					requestFields(
-						fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
-						fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
-						fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
-					),
-					responseFields(
-						fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이디"),
-						fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
-						fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
-						fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
-					)
-				)
-			);
+			.andDo(document("post-todo", getRequestPreProcessor(), getResponsePreProcessor(),
+				requestFields(fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
+					fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
+					fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")),
+				responseFields(fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이디"),
+					fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
+					fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
+					fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부"))));
 	}
 
 	@Test
@@ -105,29 +92,17 @@ public class TodoControllerTest {
 		given(mapper.todosToTodoResponse(anyList())).willReturn(responses);
 
 		// when
-		ResultActions actions =
-			mockMvc.perform(
-				get("/")
-					.accept(MediaType.APPLICATION_JSON)
-			);
+		ResultActions actions = mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON));
 
 		// then
-		actions
-			.andExpect(status().isOk())
+		actions.andExpect(status().isOk())
 			.andExpect(jsonPath("$").isArray())
 			.andExpect(jsonPath("$.length()").value(responses.size()))
-			.andDo(
-				document("get-todos",
-					getRequestPreProcessor(),
-					getResponsePreProcessor(),
-					responseFields(
-						fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("아이디"),
-						fieldWithPath("[].title").type(JsonFieldType.STRING).description("타이틀"),
-						fieldWithPath("[].todoOrder").type(JsonFieldType.NUMBER).description("번호"),
-						fieldWithPath("[].completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
-					)
-				)
-			);
+			.andDo(document("get-todos", getRequestPreProcessor(), getResponsePreProcessor(),
+				responseFields(fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("아이디"),
+					fieldWithPath("[].title").type(JsonFieldType.STRING).description("타이틀"),
+					fieldWithPath("[].todoOrder").type(JsonFieldType.NUMBER).description("번호"),
+					fieldWithPath("[].completed").type(JsonFieldType.BOOLEAN).description("완료 여부"))));
 	}
 
 	@Test
@@ -141,33 +116,19 @@ public class TodoControllerTest {
 		given(mapper.todoToTodoResponse(any(Todos.class))).willReturn(response);
 
 		// when
-		ResultActions actions =
-			mockMvc.perform(
-				get("/{id}", id)
-					.accept(MediaType.APPLICATION_JSON)
-			);
+		ResultActions actions = mockMvc.perform(get("/{id}", id).accept(MediaType.APPLICATION_JSON));
 
 		// then
-		actions
-			.andExpect(status().isOk())
+		actions.andExpect(status().isOk())
 			.andExpect(jsonPath("$.title").value(todo.getTitle()))
 			.andExpect(jsonPath("$.todoOrder").value(todo.getTodoOrder()))
 			.andExpect(jsonPath("$.completed").value(todo.getCompleted()))
-			.andDo(
-				document("get-todo",
-					getRequestPreProcessor(),
-					getResponsePreProcessor(),
-					pathParameters(
-						parameterWithName("id").description("아이디")
-					),
-					responseFields(
-						fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이디"),
-						fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
-						fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
-						fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
-					)
-				)
-			);
+			.andDo(document("get-todo", getRequestPreProcessor(), getResponsePreProcessor(),
+				pathParameters(parameterWithName("id").description("아이디")),
+				responseFields(fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이디"),
+					fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
+					fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
+					fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부"))));
 	}
 
 	@Test
@@ -186,41 +147,25 @@ public class TodoControllerTest {
 		String content = gson.toJson(patch);
 
 		// when
-		ResultActions actions =
-			mockMvc.perform(
-				patch("/{id}", id)
-					.accept(MediaType.APPLICATION_JSON)
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(content)
-			);
+		ResultActions actions = mockMvc.perform(patch("/{id}", id).accept(MediaType.APPLICATION_JSON)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(content));
 
 		// then
-		actions
-			.andExpect(status().isOk())
+		actions.andExpect(status().isOk())
 			.andExpect(jsonPath("$.id").value(patch.getId()))
 			.andExpect(jsonPath("$.title").value(patch.getTitle()))
 			.andExpect(jsonPath("$.todoOrder").value(patch.getTodoOrder()))
 			.andExpect(jsonPath("$.completed").value(patch.getCompleted()))
-			.andDo(
-				document("patch-todo",
-					getRequestPreProcessor(),
-					getResponsePreProcessor(),
-					pathParameters(
-						parameterWithName("id").description("아이디")
-					),
-					requestFields(
-						fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
-						fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
-						fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
-					),
-					responseFields(
-						fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이디"),
-						fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
-						fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
-						fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")
-					)
-				)
-			);
+			.andDo(document("patch-todo", getRequestPreProcessor(), getResponsePreProcessor(),
+				pathParameters(parameterWithName("id").description("아이디")),
+				requestFields(fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
+					fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
+					fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부")),
+				responseFields(fieldWithPath("id").type(JsonFieldType.NUMBER).description("아이디"),
+					fieldWithPath("title").type(JsonFieldType.STRING).description("타이틀"),
+					fieldWithPath("todoOrder").type(JsonFieldType.NUMBER).description("번호"),
+					fieldWithPath("completed").type(JsonFieldType.BOOLEAN).description("완료 여부"))));
 	}
 
 	@Test
@@ -229,20 +174,11 @@ public class TodoControllerTest {
 		doNothing().when(todoService).deleteTodos();
 
 		// when
-		ResultActions actions =
-			mockMvc.perform(
-				delete("/")
-			);
+		ResultActions actions = mockMvc.perform(delete("/"));
 
 		// then
-		actions
-			.andExpect(status().isNoContent())
-			.andDo(
-				document("delete-todos",
-					getRequestPreProcessor(),
-					getResponsePreProcessor()
-				)
-			);
+		actions.andExpect(status().isNoContent())
+			.andDo(document("delete-todos", getRequestPreProcessor(), getResponsePreProcessor()));
 	}
 
 	@Test
@@ -253,22 +189,11 @@ public class TodoControllerTest {
 		doNothing().when(todoService).deleteTodo(id);
 
 		// when
-		ResultActions actions =
-			mockMvc.perform(
-				delete("/{id}", id)
-			);
+		ResultActions actions = mockMvc.perform(delete("/{id}", id));
 
 		// then
-		actions
-			.andExpect(status().isNoContent())
-			.andDo(
-				document("delete-todo",
-					getRequestPreProcessor(),
-					getResponsePreProcessor(),
-					pathParameters(
-						parameterWithName("id").description("아이디")
-					)
-				)
-			);
+		actions.andExpect(status().isNoContent())
+			.andDo(document("delete-todo", getRequestPreProcessor(), getResponsePreProcessor(),
+				pathParameters(parameterWithName("id").description("아이디"))));
 	}
 }
